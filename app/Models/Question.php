d72f94470e4c2be264111ae1e6c8c0aad8f47e0b
Patrik Hrkút <Patrik.Hrkut@fri.uniza.sk>
2026-02-10 22:49:27 +0100

    Add Question model with fillable attributes and casts

---
+++ app/Models/Question.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'question',
+        'answered',
+    ];
+
+    protected $casts = [
+        'answered' => 'boolean',
+    ];
+}
